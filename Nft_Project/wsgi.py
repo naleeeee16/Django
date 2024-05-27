6ff8182886da8fb62208da3023a911c6275172df
Natalija <gn210294d@student.etf.bg.ac.rs>
2024-05-28 01:44:35 +0200

    pokusaj 4

--- Nft_Project/wsgi.py
+++ Nft_Project/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-settings_module= 'Nft_Project.deployment' if 'WEBSITE_HOSTNAEM' in os.environ else 'Nft_Project.settings'
+settings_module= 'Nft_Project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Nft_Project.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
