e123c77ee9c71940f9635887ad43f3e87fc68b18
Natalija <gn210294d@student.etf.bg.ac.rs>
2024-05-28 01:17:32 +0200

    new secret

--- Nft_Project/deployment.py
+++ Nft_Project/deployment.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS=[os.environ["WEBSITE_HOSTNAME"]]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ["WEBSITE_HOSTNAME"]]
 DEBUG =False
 
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 MIDDLEWARE = [
